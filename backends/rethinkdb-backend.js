9d7df1d95023be3e4736e4af9ff393d91f51a920
mchmarny <mark@chmarny.com>
2014-01-25 16:54:42 -0800

    Rethinkdb tested

--- backends/rethinkdb-backend.js
+++ backends/rethinkdb-backend.js
@@ -5,6 +5,16 @@
 
 	Either one will do:
 		npm install rethinkdb
+
+	Config
+
+		"handler": "./backends/rethinkdb-backend.js",
+		"config": {
+			"host": "127.0.0.1",
+			"port": 28015,
+			"db": "mqtt",
+			"table": "messages"
+		}
 		
 */
 
@@ -16,44 +26,61 @@ var RethinkDbBackend = function (config, events, logger){
 	var self = this;
 	self.config = config;
 	self.logger = logger;
-	self.dbName = config.config.db || "test";
-	self.tableName = config.config.table || "messages";
+	self.dbName = config.config.conn.db || "test";
+	self.tableName = config.config.conn.table || "messages";
+	self.connOpt = config.config.conn || {};
+	self.conn = null;
+	self.connect = function(callback){
+		if (self.conn){
+			callback(null, self.conn);
+			return;
+		}
 
-	var connOpt = config.config.conn || {};
+		logger.debug("rethinkDb connecting...");
+		r.connect(self.connOpt, function(err, conn) {
+	   	if(err) {
+		 		logger.error("Error on connect: ", err);
+		 		callback(err);
+	    	}else{
+	    		self.conn = conn;
+	    		conn['_id'] = Math.floor(Math.random()*10001);
+	    		logger.debug("Connected: ", conn['_id']);
+	    		callback(null, conn);
+	    	}
+		});
+	};
 
-	r.connect(connOpt, function(err, conn) {
-	  if(err) throw err;
-	  self.db = r.db(self.dbName);
-	  self.db.tableCreate(self.tableName).run(conn, function(err, res) {
-	    if(err) throw err;
-	    self.logger.debug("Table create result", res);
-	    events.on('data', function(topic, data){
-		 	self.save(self.db, topic, data);
-		 });
-	  });
+	events.on('data', function(topic, data){
+		self.save(topic, data);
 	});
-	
-	logger.info("Statsd backend created %j", config);
+
+	logger.info("rethinkDb backend created %j", config);
 };
 
-RethinkDbBackend.prototype.save = function(conn, topic, data) {
-	if (!topic || !data) return;
+RethinkDbBackend.prototype.save = function(topic, data) {
 	var self = this;
+	
+	if (!topic || !data) {
+		logger.error("Null argument: T:%s > ", topic, data);
+	}
 	
 	var doc = {
 		on: new Date(),
 		topic: topic,
+		topic_parts: topic.split("/"),
 		content: data
 	};
 
 	self.logger.debug("[%s]", self.config.index, doc);
 
-	r.table(self.tableName)
-	 .insert(doc)
-	 .run(conn, function(err, res){
-      if(err) self.logger.error(err);
-      else self.logger.debug(res);
-    });
+	self.connect(function(err, conn){
+		r.table(self.tableName)
+		 .insert(doc)
+		 .run(conn, function(err, res){
+	      if(err) self.logger.error(err);
+	      else self.logger.debug(res);
+	    });
+	});
 
 };
 
